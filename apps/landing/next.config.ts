88c4c1598f738be3094ad538a3683291b946bcd8
Hanbin Cho <forestream@devfive.kr>
2025-06-10 14:22:51 +0900

    Install devup-ui

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import { DevupUI } from '@devup-ui/next-plugin'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default DevupUI(nextConfig)
