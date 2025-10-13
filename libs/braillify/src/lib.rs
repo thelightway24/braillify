ecdb6e9cb3aea04af09fba0991044275c5162a8c
owjs3901 <owjs3901@gmail.com>
2025-10-13 21:23:51 +0900

    Implement cli and python cli

--- libs/braillify/src/lib.rs
+++ libs/braillify/src/lib.rs
@@ -13,6 +13,7 @@ use crate::{
 
 mod char_shortcut;
 mod char_struct;
+pub mod cli;
 mod english;
 mod jauem;
 mod korean_char;
--- packages/python/src/lib.rs
+++ packages/python/src/lib.rs
@@ -1,4 +1,6 @@
+
 use braillify as braillify_core;
+use braillify::cli::run_cli;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
@@ -17,11 +19,22 @@ fn translate_to_braille_font(text: &str) -> PyResult<String> {
     braillify_core::encode_to_braille_font(text).map_err(PyErr::new::<PyValueError, _>)
 }
 
+#[pyfunction]
+fn cli(py: Python) -> PyResult<()> {
+    run_cli(
+        py.import("sys")?
+            .getattr("argv")?
+            .extract::<Vec<String>>()?,
+    )
+    .map_err(|e| PyValueError::new_err(e.to_string()))
+}
+
 /// A Python module implemented in Rust.
 #[pymodule(name = "braillify")]
 fn lib_braillify(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(encode, m)?)?;
     m.add_function(wrap_pyfunction!(translate_to_unicode, m)?)?;
     m.add_function(wrap_pyfunction!(translate_to_braille_font, m)?)?;
+    m.add_function(wrap_pyfunction!(cli, m)?)?;
     Ok(())
 }
